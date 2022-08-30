56b8496e63479be955b3e7e602f27e783603fbef
juliocauan <juliocauan@gmail.com>
2022-08-30 15:31:19 -0300

    Changed MySQL to PostegreSQL

--- init.sql
+++ init.sql
@@ -1,3 +1,20 @@
+DROP TABLE IF EXISTS revenues;
+DROP TABLE IF EXISTS expenses;
+
+CREATE TABLE revenues(
+    id SERIAL PRIMARY KEY,
+    description VARCHAR(50) NOT NULL,
+    value DECIMAL(10, 2) NOT NULL,
+    incoming_date DATE NOT NULL
+);
+
+CREATE TABLE expenses(
+    id SERIAL PRIMARY KEY,
+    description VARCHAR(50) NOT NULL,
+    value DECIMAL(10, 2) NOT NULL,
+    outcoming_date DATE NOT NULL
+);
+
 INSERT INTO revenues(description, value, incoming_date) VALUES('Salário', 1383.14, '2022-09-15');
 INSERT INTO revenues(description, value, incoming_date) VALUES('Investimento', 116.56, '2022-09-07');
 

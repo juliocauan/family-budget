b2e39557d5897293cd45be6fdca8b488841e699e
juliocauan <juliocauan@gmail.com>
2022-09-28 15:45:18 -0300

    Turned GET and POST schemas into DTO

--- src/main/java/br/com/juliocauan/familybudget/controller/RevenuesController.java
+++ src/main/java/br/com/juliocauan/familybudget/controller/RevenuesController.java
@@ -12,50 +12,49 @@ import org.springframework.web.bind.annotation.RestController;
 import br.com.juliocauan.familybudget.infrastructure.application.dao.RevenueEntityDAO;
 import br.com.juliocauan.familybudget.infrastructure.application.model.RevenueEntity;
 import br.com.juliocauan.openapi.api.RevenuesApi;
-import br.com.juliocauan.openapi.model.RevenueGET;
-import br.com.juliocauan.openapi.model.RevenuePOST;
+import br.com.juliocauan.openapi.model.RevenueDTO;
 
 @RestController
 public class RevenuesController implements RevenuesApi{
 
     @Override
-    public ResponseEntity<Void> _postRevenue(@Valid RevenuePOST revenuePOST) {
+    public ResponseEntity<Void> _postRevenue(@Valid RevenueDTO revenuePOST) {
         RevenueEntityDAO revenueDao = new RevenueEntityDAO();
         revenueDao.save(dtoToEntity(revenuePOST));
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
     @Override
-    public ResponseEntity<List<RevenueGET>> _getAllRevenues() {
+    public ResponseEntity<List<RevenueDTO>> _getAllRevenues() {
         RevenueEntityDAO revenueDAO = new RevenueEntityDAO();
         List<RevenueEntity> list = revenueDAO.getAll();
-        List<RevenueGET> response = new ArrayList<>();
+        List<RevenueDTO> response = new ArrayList<>();
         list.forEach(revenue -> response.add(entityToDto(revenue)));
         return ResponseEntity.status(HttpStatus.OK).body(response);
     }
 
     @Override
-    public ResponseEntity<RevenueGET> _getRevenue(Integer revenueId) {
+    public ResponseEntity<RevenueDTO> _getRevenue(Integer revenueId) {
         RevenueEntityDAO revenueDAO = new RevenueEntityDAO();
         RevenueEntity entity = revenueDAO.findOne(revenueId);
-        RevenueGET response = entityToDto(entity);
+        RevenueDTO response = entityToDto(entity);
         return ResponseEntity.status(HttpStatus.OK).body(response);
     }
 
     @Override
-    public ResponseEntity<Void> _updateRevenue(Integer revenueId, @Valid RevenuePOST revenuePOST) {
+    public ResponseEntity<Void> _updateRevenue(Integer revenueId, @Valid RevenueDTO revenuePOST) {
         // TODO Auto-generated method stub
         return null;
     }
 
-    private RevenueGET entityToDto(RevenueEntity entity){
-        return new RevenueGET()
+    private RevenueDTO entityToDto(RevenueEntity entity){
+        return new RevenueDTO()
             .description(entity.getDescription())
             .value(entity.getValue())
             .date(entity.getIncomeDate());
     }
 
-    private RevenueEntity dtoToEntity(RevenuePOST dto){
+    private RevenueEntity dtoToEntity(RevenueDTO dto){
         return RevenueEntity.builder()
             .description(dto.getDescription())
             .value(dto.getValue())

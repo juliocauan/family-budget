b73dd31735b78f09d55a14d068ce454405e2d638
juliocauan <juliocauan@gmail.com>
2022-10-27 15:16:54 -0300

    Added category field in Expenses db init

--- docker-resources/init.sql
+++ docker-resources/init.sql
@@ -12,11 +12,12 @@ CREATE TABLE expenses(
     id SERIAL PRIMARY KEY,
     description VARCHAR(50) NOT NULL,
     quantity DECIMAL(10, 2) NOT NULL,
-    outcome_date DATE NOT NULL
+    outcome_date DATE NOT NULL,
+    category VARCHAR(15)
 );
 
 INSERT INTO revenues(description, quantity, income_date) VALUES('Salário', 1383.14, '2022-09-15');
 INSERT INTO revenues(description, quantity, income_date) VALUES('Investimento', 116.56, '2022-09-07');
 
-INSERT INTO expenses(description, quantity, outcome_date) VALUES('Intercâmbio', 999.95, '2022-09-18');
-INSERT INTO expenses(description, quantity, outcome_date) VALUES('Academia', 120.00, '2022-09-05');
+INSERT INTO expenses(description, quantity, outcome_date, category) VALUES('Intercâmbio', 999.95, '2022-09-18', 'EDUCATION');
+INSERT INTO expenses(description, quantity, outcome_date, category) VALUES('Academia', 120.00, '2022-09-05', 'HEALTH');

5605dc5a1e58a7e9ed8d70fc4629483f96b191f4
juliocauan <juliocauan@gmail.com>
2022-11-10 12:41:31 -0300

    Renamed Schemas

--- docker-resources/init.sql
+++ docker-resources/init.sql
@@ -1,19 +1,20 @@
-CREATE SCHEMA IF NOT EXISTS fb;
+CREATE SCHEMA IF NOT EXISTS application;
+CREATE SCHEMA IF NOT EXISTS auth;
 
-DROP TABLE IF EXISTS fb.revenues;
-DROP TABLE IF EXISTS fb.expenses;
-DROP TABLE IF EXISTS fb.users;
-DROP TABLE IF EXISTS fb.roles;
-DROP TABLE IF EXISTS fb.users_roles;
+DROP TABLE IF EXISTS application.revenues;
+DROP TABLE IF EXISTS application.expenses;
+DROP TABLE IF EXISTS auth.users;
+DROP TABLE IF EXISTS auth.roles;
+DROP TABLE IF EXISTS auth.users_roles;
 
-CREATE TABLE fb.revenues(
+CREATE TABLE application.revenues(
     id SERIAL PRIMARY KEY,
     description VARCHAR(50) NOT NULL,
     quantity DECIMAL(10, 2) NOT NULL,
     income_date DATE NOT NULL
 );
 
-CREATE TABLE fb.expenses(
+CREATE TABLE application.expenses(
     id SERIAL PRIMARY KEY,
     description VARCHAR(50) NOT NULL,
     quantity DECIMAL(10, 2) NOT NULL,
@@ -21,34 +22,34 @@ CREATE TABLE fb.expenses(
     category VARCHAR(15)
 );
 
-CREATE TABLE fb.users(
+CREATE TABLE auth.users(
     id UUID PRIMARY KEY DEFAULT gen_random_uuid (),
     name VARCHAR(128) NOT NULL,
     email VARCHAR(128) UNIQUE NOT NULL,
     secret VARCHAR(255) NOT NULL
 );
 
-CREATE TABLE fb.roles(
+CREATE TABLE auth.roles(
     id SMALLSERIAL PRIMARY KEY,
     name VARCHAR(15) NOT NULL
 );
 
-CREATE TABLE fb.users_roles(
-    user_id UUID REFERENCES fb.users(id),
-    role_id SMALLINT REFERENCES fb.roles(id),
+CREATE TABLE auth.users_roles(
+    user_id UUID REFERENCES auth.users(id),
+    role_id SMALLINT REFERENCES auth.roles(id),
     PRIMARY KEY(user_id, role_id)
 );
 
-INSERT INTO fb.revenues(description, quantity, income_date) VALUES('Salário', 1383.14, '2022-09-15');
-INSERT INTO fb.revenues(description, quantity, income_date) VALUES('Investimento', 116.56, '2022-09-07');
+INSERT INTO application.revenues(description, quantity, income_date) VALUES('Salário', 1383.14, '2022-09-15');
+INSERT INTO application.revenues(description, quantity, income_date) VALUES('Investimento', 116.56, '2022-09-07');
 
-INSERT INTO fb.expenses(description, quantity, outcome_date, category) VALUES('Intercâmbio', 999.95, '2022-09-18', 'EDUCATION');
-INSERT INTO fb.expenses(description, quantity, outcome_date, category) VALUES('Academia', 120.00, '2022-09-05', 'HEALTH');
+INSERT INTO application.expenses(description, quantity, outcome_date, category) VALUES('Intercâmbio', 999.95, '2022-09-18', 'EDUCATION');
+INSERT INTO application.expenses(description, quantity, outcome_date, category) VALUES('Academia', 120.00, '2022-09-05', 'HEALTH');
 
-INSERT INTO fb.users(name, email, secret) VALUES
+INSERT INTO auth.users(name, email, secret) VALUES
     ('Julio', 'julio@test.com', '$2a$10$ofEy..aODV5QleKty0kkJ.8UXdOXIdr/CeyXswcjJGBVYgxU296NK');
-INSERT INTO fb.users(name, email, secret) VALUES
+INSERT INTO auth.users(name, email, secret) VALUES
     ('Cauan', 'cauan@test.com', '$2a$10$ofEy..aODV5QleKty0kkJ.8UXdOXIdr/CeyXswcjJGBVYgxU296NK');
 
-INSERT INTO fb.roles(name) VALUES ('ADMIN');
-INSERT INTO fb.roles(name) VALUES ('CLIENT');
+INSERT INTO auth.roles(name) VALUES ('ADMIN');
+INSERT INTO auth.roles(name) VALUES ('CLIENT');

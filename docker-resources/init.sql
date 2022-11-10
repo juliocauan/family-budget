9e9a2c08b67020796b0864a52a370d0b1bd12cd3
juliocauan <juliocauan@gmail.com>
2022-11-10 12:09:38 -0300

    Created Roles table

--- docker-resources/init.sql
+++ docker-resources/init.sql
@@ -3,6 +3,7 @@ CREATE SCHEMA IF NOT EXISTS fb;
 DROP TABLE IF EXISTS fb.revenues;
 DROP TABLE IF EXISTS fb.expenses;
 DROP TABLE IF EXISTS fb.users;
+DROP TABLE IF EXISTS fb.roles;
 
 CREATE TABLE fb.revenues(
     id SERIAL PRIMARY KEY,
@@ -22,8 +23,13 @@ CREATE TABLE fb.expenses(
 CREATE TABLE fb.users(
     id UUID PRIMARY KEY DEFAULT gen_random_uuid (),
     name VARCHAR(128) NOT NULL,
-    email VARCHAR(128) UNIQUE,
+    email VARCHAR(128) UNIQUE NOT NULL,
     secret VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE fb.roles(
+    id SMALLSERIAL PRIMARY KEY,
+    name VARCHAR(15) NOT NULL
 );
 
 INSERT INTO fb.revenues(description, quantity, income_date) VALUES('Salário', 1383.14, '2022-09-15');
@@ -32,7 +38,10 @@ INSERT INTO fb.revenues(description, quantity, income_date) VALUES('Investimento
 INSERT INTO fb.expenses(description, quantity, outcome_date, category) VALUES('Intercâmbio', 999.95, '2022-09-18', 'EDUCATION');
 INSERT INTO fb.expenses(description, quantity, outcome_date, category) VALUES('Academia', 120.00, '2022-09-05', 'HEALTH');
 
-INSERT INTO fb.users (name, email, secret) VALUES
+INSERT INTO fb.users(name, email, secret) VALUES
     ('Julio', 'julio@test.com', '$2a$10$ofEy..aODV5QleKty0kkJ.8UXdOXIdr/CeyXswcjJGBVYgxU296NK');
-INSERT INTO fb.users (name, email, secret) VALUES
+INSERT INTO fb.users(name, email, secret) VALUES
     ('Cauan', 'cauan@test.com', '$2a$10$ofEy..aODV5QleKty0kkJ.8UXdOXIdr/CeyXswcjJGBVYgxU296NK');
+
+INSERT INTO fb.roles(name) VALUES ('ADMIN');
+INSERT INTO fb.roles(name) VALUES ('CLIENT');
